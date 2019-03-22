dac6609a02be96b46e53b645f276fd8efe2d5127
tryingtokeepup <kl.hawaii@gmail.com>
2019-03-22 08:19:18 -0700

    finished first OOP file, will test.

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,49 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+class Vehicle:
+    def __init__(self):
+        # this is the base class, Vehicle, which will be the base for all other classes.
+        pass
+
+
+class FlightVehicle(Vehicle):
+    def __init__(self):
+        # this will be the base class for Starship and Airplane
+        pass
+
+# Flight Vehicle classes and children
+
+
+class Starship(FlightVehicle):
+    def __init__(self):
+        # this will inherit from FlightVehicle
+        pass
+
+
+class Airplane(FlightVehicle):
+    def __init__(self):
+        # this will also inherit from FlightVehicle
+        pass
+
+# Ground Vehicle classes and children
+
+
+class GroundVehicle(Vehicle):
+    def __init__(self):
+        # this will serve as the base class for the other ground vehicles, and inherits from Vehicle
+        pass
+
+
+class Car(GroundVehicle):
+    def __init__(self):
+        # inherits from GroundVehicle
+        pass
+
+
+class Motocycle(GroundVehicle):
+    def __init__(self):
+        # inherits from GroundVehicle
+        pass
